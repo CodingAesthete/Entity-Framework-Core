699eb593085152b93428357756440f2028ead2b4
PostPunkAesthete <harry_ollI@abv.bg>
2022-04-21 20:04:21 +0300

    Initial commit

---
+++ 02.Entity Framework Core Intro - Exercise/02.Database First/StartUp.cs
@@ -0,0 +1,12 @@
+namespace SoftUni
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
